38638a30322e7242b88d2ee92ae7dd3c305fd776
Dora Dev <dr007298@cfjl.com.br>
2026-03-19 17:18:55 -0300

    código estava errado ajeitei

--- ardu.ino
+++ ardu.ino
@@ -1,44 +1,24 @@
-//Preguiça de fazer o código, só pra não deixar o arquivo vazio(Dora) :)
+//Variáveis
+int leds [ 16 ] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26}; // Array para os pinos dos LEDs(Dora)
+int motores [ 16 ] = {31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46}; // Array para os pinos dos motores(Dora)
+
+
+
+
 void setup() {
-    // LEDs
-    int led01 = 11;
-    int led02 = 12;
-    int led03 = 13;
-    int led04 = 14;
-    int led05 = 15;
-    int led06 = 16;
-    int led07 = 17;
-    int led08 = 18;
-    int led09 = 19;
-    int led10 = 20;
-    int led11 = 21;
-    int led12 = 22;
-    int led13 = 23;
-    int led14 = 24;
-    int led15 = 25;
-    int led16 = 26;
-    // Motores
-    int motor01 = 31;
-    int motor02 = 32;
-    int motor03 = 33;
-    int motor04 = 34;
-    int motor05 = 35;
-    int motor06 = 36;
-    int motor07 = 37;
-    int motor08 = 38;
-    int motor09 = 39;
-    int motor10 = 40;
-    int motor11 = 41;
-    int motor12 = 42;
-    int motor13 = 43;
-    int motor14 = 44;
-    int motor15 = 45;
-    int motor16 = 46;
+    //For que configura os outputs dos LEDS(Dora)
+    for (int i = 0; i < 16; i++) {
+        pinMode(leds[i], OUTPUT);
+    }
+    //For que configura os outputs dos motores(Dora)
+    for (int i = 0; i < 16; i++) {
+        pinMode(motores[i], OUTPUT);
+    }
 }
 
-void loop() {
-    // Compartimento selecionado
-    int compartimentoSelecionado = 0;   
 
+
+
+void loop() {
 
 }
