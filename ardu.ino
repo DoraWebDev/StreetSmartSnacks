c68848477a046422feabb491cf5b7c47cb43103f
Dora Dev <dr007298@cfjl.com.br>
2026-03-18 21:09:17 -0300

    Update ardu.ino

--- ardu.ino
+++ ardu.ino
@@ -1,8 +1,44 @@
 //Preguiça de fazer o código, só pra não deixar o arquivo vazio(Dora) :)
 void setup() {
-
+    // LEDs
+    int led01 = 11;
+    int led02 = 12;
+    int led03 = 13;
+    int led04 = 14;
+    int led05 = 15;
+    int led06 = 16;
+    int led07 = 17;
+    int led08 = 18;
+    int led09 = 19;
+    int led10 = 20;
+    int led11 = 21;
+    int led12 = 22;
+    int led13 = 23;
+    int led14 = 24;
+    int led15 = 25;
+    int led16 = 26;
+    // Motores
+    int motor01 = 31;
+    int motor02 = 32;
+    int motor03 = 33;
+    int motor04 = 34;
+    int motor05 = 35;
+    int motor06 = 36;
+    int motor07 = 37;
+    int motor08 = 38;
+    int motor09 = 39;
+    int motor10 = 40;
+    int motor11 = 41;
+    int motor12 = 42;
+    int motor13 = 43;
+    int motor14 = 44;
+    int motor15 = 45;
+    int motor16 = 46;
 }
 
 void loop() {
+    // Compartimento selecionado
+    int compartimentoSelecionado = 0;   
+
 
 }
